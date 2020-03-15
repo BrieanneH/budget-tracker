fc4a9e4910a794f9824aaa7d2fe15a3a98628d05
BrieanneH <howard.brieanne@gmail.com>
2020-03-15 00:31:20 -0500

    restructuring server.js, work in progress

--- server.js
+++ server.js
@@ -5,20 +5,21 @@ const compression = require("compression");
 
 
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(compression());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+
+
 
 db.on("error", error => {
     console.log("error")
 });
-//at least psuedo coding get, post, delete, will fill in when I know what the -- I'm doing 
+
 app.get("/", (req, res)=>{
 
 });
