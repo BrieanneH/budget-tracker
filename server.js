467ae74db750956aebd467d1cee43e51b67219e3
BrieanneH <howard.brieanne@gmail.com>
2020-04-07 04:52:14 -0500

    pushing updates

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const compression = require("compression");
-
+const mongojs = require('mongojs');
+const path = require("path");
+require('dotenv').config();
 
 
 const PORT = process.env.PORT || 3000;
@@ -15,20 +17,36 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/transaction", {
+
+const MONGODB_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds231725.mlab.com:31725/heroku_hsvxk2zd`
+console.log(MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget-tracker", {
   useNewUrlParser: true,
-  useFindandModify: false
+  useFindandModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
+// set up mongo db
+const databaseURL = process.env.MONGODB_URI || "budget-tracker"
+const collections = ["transaction"]
 
+
+// reference mongo db
+const db = mongojs(databaseURL, collections)
+
+// if error with db, throw error
+db.on("error", error => {
+  console.log(`db error: ${error}`)
+})
 
 //getting routes
-app.use(require('routes/api.js'));
+app.use(require('./routes/api.js'));
 
 
 
 
 app.listen(PORT,() => {
-  console.log(`Now listening on port: ${PORT}!!`);
+  console.log(`Now listening on port: http://localhost:${PORT}`);
 });
 
